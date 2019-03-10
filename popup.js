3c3803b9b4de1f8ae31f3a02515407ede425f8fa
chandlerlei2017 <chandlerlei87@gmail.com>
2019-03-10 13:58:59 -0400

    add temp popup

--- popup.js
+++ popup.js
@@ -1,17 +1,53 @@
-var signin = function (callback) {
-  chrome.identity.getAuthToken({interactive: true}, callback);
-};
-
-function onGoogleLibraryLoaded() {
-  signin(authorizationCallback);
+function popupClicked(tab) {
+  getAuthToken({
+      'interactive': false,
+      'callback': AuthTokenCallback,
+  });
 }
 
-var authorizationCallback = function (data) {
-  gapi.auth.setToken({access_token: data});
-  gapi.client.load("https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest");
-  onLoad();
-};
-
-function onLoad(){
-  gapi.client.gmail.users.getProfile["emailAddress"];
+function AuthTokenCallback(token) {
+  if (chrome.runtime.lastError) {
+      getAuthTokenInteractive();
+  }
 }
+
+function getAuthTokenInteractive() {
+  getAuthToken({
+      'interactive': true,
+      'callback': getAuthTokenInteractiveCallback,
+  });
+}
+
+function getAuthTokenInteractiveCallback(token) {
+  // Catch chrome error if user is not authorized.
+  if (chrome.runtime.lastError) {
+      showAuthNotification();
+  }
+}
+
+function getAuthToken(options) {
+  chrome.identity.getAuthToken({ 'interactive': options.interactive }, options.callback);
+}
+
+function showAuthNotification() {
+  var options = {
+      'id': 'start-auth',
+      'iconUrl': 'img/developers-logo.png',
+      'title': 'GDE Sample: Chrome extension Google APIs',
+      'message': 'Click here to authorize access to Gmail',
+  };
+  createBasicNotification(options);
+}
+
+function createBasicNotification(options) {
+  var notificationOptions = {
+      'type': 'basic',
+      'iconUrl': options.iconUrl, // Relative to Chrome dir or remote URL must be whitelisted in manifest.
+      'title': options.title,
+      'message': options.message,
+      'isClickable': true,
+  };
+  chrome.notifications.create(options.id, notificationOptions, function(notificationId) {});
+}
+
+popupClicked();
